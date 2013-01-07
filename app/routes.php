bd4a9942d312ebc9457ed93c6fdd76acec5adb22
Anahkiasen <ehtnam6@gmail.com>
2013-01-07 20:31:52 +0100

    Add home view

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+//////////////////////////////////////////////////////////////////////
+////////////////////////////// NAMESPACES ////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
-Route::get('/', function()
-{
-	return View::make('hello');
+View::addLocation(__DIR__.'/views/layouts');
+View::addLocation(__DIR__.'/views/partials');
+
+//////////////////////////////////////////////////////////////////////
+/////////////////////////////// ROUTES ///////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+Route::any('/', function() {
+  return View::make('home');
 });
---
+++ app/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+  @include('about')
+  <hr>
+  @include('categories')
+@stop
