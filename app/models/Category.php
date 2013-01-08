ba1c6e99f0eb03c97e235180ac0bea4b859e8bda
Anahkiasen <ehtnam6@gmail.com>
2013-01-09 00:29:21 +0100

    Add Category->thumb method

--- app/models/Category.php
+++ app/models/Category.php
@@ -3,4 +3,12 @@
 class Category extends Eloquent
 {
   protected $table = 'categories';
+
+  /**
+   * Get the category's thumb
+   */
+  public function getThumb()
+  {
+    return HTML::image('app/img/categories/'.$this->id.'.jpg', $this->name);
+  }
 }
