04c6785dba267e5f14887619fc838af934b8e106
Anahkiasen <ehtnam6@gmail.com>
2013-01-10 17:56:44 +0100

    Tweak tests

--- app/tests/RoutesTest.php
+++ app/tests/RoutesTest.php
@@ -52,31 +52,4 @@ class RoutesTest extends TestCase
     $articles = sizeof($crawler->filter('.articles article'));
     $this->assertNotEquals(0, $articles, 'No articles found in this category');
   }
-
-  // Helpers ------------------------------------------------------- /
-
-  /**
-   * Get the Crawler for a page
-   *
-   * @param string $url The page url
-   *
-   * @return Crawler
-   */
-  private function getPage($url)
-  {
-    $crawler = $this->client->request('GET', '/'.$url);
-    $this->assertTrue($this->client->getResponse()->isOk());
-
-    return $crawler;
-  }
-
-  /**
-   * Check if a page is correctly loaded
-   */
-  private function assertIsPage($url, $title)
-  {
-    $crawler = $this->getPage($url);
-
-    $this->assertCount(1, $crawler->filter('title:contains("' .$title. '")'), "The request page $title couldn't be reacher");
-  }
 }
--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,19 +1,45 @@
 <?php
+class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
+  /**
+   * Creates the application.
+   *
+   * @return Symfony\Component\HttpKernel\HttpKernelInterface
+   */
+  public function createApplication()
+  {
+  	$unitTesting = true;
+    $testEnvironment = 'testing';
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase {
+  	return require __DIR__.'/../../start.php';
+  }
 
-    /**
-     * Creates the application.
-     *
-     * @return Symfony\Component\HttpKernel\HttpKernelInterface
-     */
-    public function createApplication()
-    {
-    	$unitTesting = true;
+  ////////////////////////////////////////////////////////////////////
+  //////////////////////////// HELPERS ///////////////////////////////
+  ////////////////////////////////////////////////////////////////////
 
-        $testEnvironment = 'testing';
+  /**
+   * Get the Crawler for a page
+   *
+   * @param string $url The page url
+   *
+   * @return Crawler
+   */
+  protected function getPage($url)
+  {
+    $crawler = $this->client->request('GET', '/'.$url);
+    $this->assertTrue($this->client->getResponse()->isOk());
 
-    	return require __DIR__.'/../../start.php';
-    }
+    return $crawler;
+  }
 
+  /**
+   * Check if a page is correctly loaded
+   */
+  protected function assertIsPage($url, $title)
+  {
+    $crawler = $this->getPage($url);
+
+    $this->assertCount(1, $crawler->filter('title:contains("' .$title. '")'), "The request page $title couldn't be reacher");
+  }
 }
