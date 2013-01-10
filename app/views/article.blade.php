4aeb1f4a842b411d3e0bb1287b7fb9d8d1c2a692
Anahkiasen <ehtnam6@gmail.com>
2013-01-10 01:38:54 +0100

    Add article view

---
+++ app/views/article.blade.php
@@ -0,0 +1,30 @@
+@extends('global')
+
+@section('title')
+  {{ $article->name }} -
+@stop
+
+@section('css')
+  {{ Basset::show('article.css') }}
+@stop
+
+@section('navigation')
+  {{ HTML::homeLink() }}
+  {{ HTML::backLink(route('category', array('slug' => $category->id)), 'Retour à '.$category->name) }}
+@stop
+
+@section('layout')
+  <section class='article @if($article->image) novel @endif'>
+    <h1>{{ $article->name }}</h1>
+    <article>
+      {{ $article->content }}
+      <blockquote>
+        <strong>Publié le : {{ $article->created_at }}</strong>
+      </blockquote>
+    </article>
+  </section>
+@stop
+
+@section('js')
+  {{ Basset::show('article.js') }}
+@stop
