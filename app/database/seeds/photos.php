8b8a8175b9a73743141e26abad967e7701c0056f
Anahkiasen <ehtnam6@gmail.com>
2013-01-11 14:26:30 +0100

    Seed photos

---
+++ app/database/seeds/photos.php
@@ -0,0 +1,32 @@
+<?php
+
+// Get all collections from my Flickr user
+$sets = Flickering::photosetsGetList('31667913@N06');
+$photosets = Arrays::each($sets->results()->photoset, function($photoset) { return $photoset['id']; });
+foreach ($photosets as $photoset_id) {
+
+  // Get all photos from this set
+  $_photos = Flickering::photosetsGetPhotos($photoset_id);
+  $_photos = $_photos->results()->photo;
+
+  // Get all sets ids from there
+  $photos = Arrays::each($_photos, function($photo) use ($photoset_id) {
+
+    $name    = $photo['title'];
+    $surname = preg_replace('/[0-9a-z]+ (- )?\(?([a-zA-Zàèé\' ]+)\)?/', '$2', $name);
+    if (preg_match('/[0-9]{1,2}[a-z]{0,2}/', $surname)) $surname = '';
+
+    return array(
+      'id'          => $photo['id'],
+      'name'        => $name,
+      'surname'     => $surname,
+      'farm'        => 'http://farm' .$photo['farm']. '.staticflickr.com/' .$photo['server']. '/' .$photo['id']. '_'.$photo['secret'],
+      'thumbnail'   => $photo['isprimary'],
+      'photoset_id' => $photoset_id,
+      'created_at'  => new DateTime,
+      'updated_at'  => new DateTime,
+    );
+  });
+}
+
+return $photos;
