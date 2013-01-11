47586681fea735bfaed85d5bc3f77a0b6c3aa09a
Anahkiasen <ehtnam6@gmail.com>
2013-01-11 14:18:04 +0100

    Seed photosets

---
+++ app/database/seeds/photosets.php
@@ -0,0 +1,14 @@
+<?php
+
+// Get all collections from my Flickr user
+$sets = Flickering::photosetsGetList('31667913@N06');
+
+return Arrays::each($sets->results()->photoset, function($photoset) {
+  return [
+    'id'         => $photoset['id'],
+    'name'       => $photoset['title']['_content'],
+    'slug'       => String::slugify($photoset['title']['_content']),
+    'created_at' => $photoset['date_create'],
+    'updated_at' => $photoset['date_update'],
+  ];
+});
