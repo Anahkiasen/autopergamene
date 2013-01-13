46e571c259b2cb30336bdea35b347d0eb3f993b2
Anahkiasen <ehtnam6@gmail.com>
2013-01-13 17:08:11 +0100

    Update Laravel

--- start.php
+++ start.php
@@ -56,6 +56,17 @@ $env = $app->detectEnvironment(array(
 |
 */
 
-require __DIR__.'/vendor/illuminate/foundation/src/start.php';
+require $app->getBootstrapFile();
+
+/*
+|--------------------------------------------------------------------------
+| Return The Application
+|--------------------------------------------------------------------------
+|
+| This script returns the application instance. The instance is given to
+| the calling script so we can separate the building of the instances
+| from the actual running of the application and sending responses.
+|
+*/
 
 return $app;
