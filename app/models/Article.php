384c39111893ce93e68bd60dd4286e1648062e57
Anahkiasen <ehtnam6@gmail.com>
2013-02-01 10:26:52 +0100

    Add some comments

--- app/models/Article.php
+++ app/models/Article.php
@@ -3,6 +3,11 @@ use Underscore\Parse;
 
 class Article extends Eloquent
 {
+  /**
+   * Get the imploded tags of the article
+   *
+   * @return string
+   */
   public function giveTags()
   {
     $tags = $this->getOriginal('tags');
