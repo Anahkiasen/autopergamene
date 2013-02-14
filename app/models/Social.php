f4cb4cb2867e95bc00934378d4112cbf9bcc6d36
Anahkiasen <ehtnam6@gmail.com>
2013-02-14 14:00:08 +0100

    Add styled link-name for Social services

--- app/models/Social.php
+++ app/models/Social.php
@@ -1,4 +1,20 @@
 <?php
 class Service extends Eloquent
 {
+
+  // Attributes ---------------------------------------------------- /
+
+  /**
+   * Get the service's link as styled name
+   *
+   * @return string
+   */
+  public function getLinkNameAttribute()
+  {
+    $linkName = String::remove($this->link, '/anahkiasen');
+    $linkName = String::remove($linkName, 'http://');
+    $linkName = String::remove($linkName, 'www.');
+
+    return $linkName;
+  }
 }
