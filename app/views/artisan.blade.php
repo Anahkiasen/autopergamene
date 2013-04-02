a7352af1eb10b89cf203e4cdb648f4ff02ee10ff
Anahkiasen <ehtnam6@gmail.com>
2013-04-02 21:43:15 +0200

    Fix HTML::style

--- app/views/artisan.blade.php
+++ app/views/artisan.blade.php
@@ -3,7 +3,7 @@
   <head>
     <title>Artisan</title>
     {{ HTML::favicon('app/img/favicon.jpg') }}
-    {{ HTML::style('components/bootstrap/docs/assets/css/bootstrap.css') }}
+    <link href="{{ URL::asset('components/bootstrap/docs/assets/css/bootstrap.css') }}" type="text/css" rel="stylesheet">
   </head>
   <body style='padding: 2rem'>
     <blockquote>
