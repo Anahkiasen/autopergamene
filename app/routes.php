863fc21060181b1fdc2bb0f591d7ce95d332ccc4
Maxime Fabre <ehtnam6@gmail.com>
2013-07-26 17:07:33 +0200

    Work on localization

--- app/routes.php
+++ app/routes.php
@@ -4,20 +4,32 @@
 /////////////////////////////// ROUTES ///////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-Route::get('/', 'CategoriesController@categories');
+$routes = function () {
+	Route::get('/', 'CategoriesController@categories');
 
-Route::get('category/graceful-degradation', 'RepositoriesController@repositories');
+	Route::get('category/graceful-degradation', 'RepositoriesController@repositories');
 
-Route::get('category/en-averse-dencre',                   'ArticlesController@articles');
-Route::get('category/en-averse-dencre/article/{article}', 'ArticlesController@article');
+	Route::get('category/en-averse-dencre',                   'ArticlesController@articles');
+	Route::get('category/en-averse-dencre/article/{article}', 'ArticlesController@article');
 
-Route::get('category/memorabilia',                  'PhotographiesController@collections');
-Route::get('category/memorabilia/album/{photoset}', 'PhotographiesController@photoset');
+	Route::get('category/memorabilia',                  'PhotographiesController@collections');
+	Route::get('category/memorabilia/album/{photoset}', 'PhotographiesController@photoset');
 
-Route::get('category/les-fleurs-davril',               'StoriesController@stories');
-Route::get('category/les-fleurs-davril/story/{story}', 'StoriesController@story');
+	Route::get('category/les-fleurs-davril',               'StoriesController@stories');
+	Route::get('category/les-fleurs-davril/story/{story}', 'StoriesController@story');
 
-Route::get('category/illustration',                   'IllustrationsController@supports');
-Route::get('category/illustration/support/{support}', 'IllustrationsController@support');
+	Route::get('category/illustration',                   'IllustrationsController@supports');
+	Route::get('category/illustration/support/{support}', 'IllustrationsController@support');
 
-Route::get('category/{category}', 'CategoriesController@category');
+	Route::get('category/{category}', 'CategoriesController@category');
+};
+
+Config::set('app.locale', Request::segment(1) ?: 'fr');
+
+Route::get('/', function() {
+	return Redirect::to(Config::get('app.locale'));
+});
+
+// Languages
+Route::group(['prefix' => 'fr'], $routes);
+Route::group(['prefix' => 'en'], $routes);
