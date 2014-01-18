65e05923e5785e95b46b2542f2e31ab5ea10d4a3
Maxime Fabre <ehtnam6@gmail.com>
2014-01-18 18:17:59 +0100

    Setup Arrounded

---
+++ app/config/testing/database.php
@@ -0,0 +1,11 @@
+<?php return array(
+
+	'connections' => array(
+		'sqlite' => array(
+			'driver'   => 'sqlite',
+			'database' => ':memory:',
+			'prefix'   => '',
+		),
+	),
+
+);
--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,81 +1,14 @@
 <?php
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase
+class TestCase extends Arrounded\Testing\TestCase
 {
   /**
-   * Creates the application.
+   * Some aliases for mocks
    *
-   * @return Symfony\Component\HttpKernel\HttpKernelInterface
+   * @var array
    */
-  public function createApplication()
-  {
-    $unitTesting     = true;
-    $testEnvironment = 'testing';
-
-    return require __DIR__.'/../../bootstrap/start.php';
-  }
-
-  ////////////////////////////////////////////////////////////////////
-  //////////////////////////////// COLORS ////////////////////////////
-  ////////////////////////////////////////////////////////////////////
-
-  /**
-   * Print an info
-   *
-   * @param  string $message
-   *
-   * @return string
-   */
-  public function line($message)
-  {
-    print $message.PHP_EOL;
-  }
-
-  /**
-   * Print an info
-   *
-   * @param  string $message
-   *
-   * @return string
-   */
-  public function success($message)
-  {
-    $this->line("\033[0;32m" .$message. "\033[0m");
-  }
-
-  /**
-   * Print an info
-   *
-   * @param  string $message
-   *
-   * @return string
-   */
-  public function info($message)
-  {
-    $this->line("\033[0;34m" .$message. "\033[0m");
-  }
-
-  /**
-   * Print an error
-   *
-   * @param  string $message
-   *
-   * @return string
-   */
-  public function error($message)
-  {
-    $this->line("\033[0;31m" .$message. "\033[0m");
-  }
-
-  /**
-   * Print a comment
-   *
-   * @param  string $message
-   *
-   * @return string
-   */
-  public function comment($message)
-  {
-    $this->line("\033[0;35m" .$message. "\033[0m");
-  }
+  protected $namespaces = array(
+    'app'    => 'Autopergamene',
+    'models' => 'Autopergamene',
+  );
 }
