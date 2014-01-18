50c63b1aa74aba47b07a6d31d81e91872259138c
Maxime Fabre <ehtnam6@gmail.com>
2014-01-18 18:25:47 +0100

    Update deploy procedure

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -5,14 +5,6 @@
 //////////////////////////////////////////////////////////////////////
 
 Rocketeer::after('deploy', function ($task) {
-	$task->command->comment('Installing Bower components');
-	$task->runForCurrentRelease('bower install --allow-root');
-
-	$task->command->comment('Installing NPM modules');
-	$task->runForCurrentRelease('npm install');
-
-	$task->command->comment('Building Basset containers');
-	$task->runForCurrentRelease('php artisan basset:build -f -p');
-
-	$task->runForCurrentRelease('chown www-data app/storage/meta/collections.json');
+	$task->command->comment('Building assets');
+	$task->runForCurrentRelease(['npm install', 'node node_modules/.bin/bower install', 'node node_modules/.bin/grunt production']);
 });
