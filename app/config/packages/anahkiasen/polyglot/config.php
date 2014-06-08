e3a670c323b0a8aed219eabc25b0707ad7508f1f
Maxime Fabre <ehtnam6@gmail.com>
2014-06-08 16:20:57 +0200

    Update polyglot configuration

--- app/config/packages/anahkiasen/polyglot/config.php
+++ app/config/packages/anahkiasen/polyglot/config.php
@@ -1,10 +1,30 @@
 <?php return array(
 
-	// The default locale if none is provided
+	// Locales
+	////////////////////////////////////////////////////////////////////
+
+	// The default locale if none is provided in the URL
+	// Leave empty to force the use of locales prefixes in URLs
 	'default' => 'fr',
+
+	// The fallback locale for translations
+	// If null, the default locale is used
+	'fallback' => null,
 
 	// The available locales
 	'locales' => array('fr', 'en'),
+
+	// Gettext
+	////////////////////////////////////////////////////////////////////
+
+	// The domain of your translations, for gettext use
+	'domain' => 'autopergamene',
+
+	// Where the PO/MO files reside
+	'folder' => app_path('lang'),
+
+	// Database
+	////////////////////////////////////////////////////////////////////
 
 	// The pattern Polyglot should follow to find the Lang classes
 	// Examples are "Lang\{model}", "{model}Lang", where {model}
